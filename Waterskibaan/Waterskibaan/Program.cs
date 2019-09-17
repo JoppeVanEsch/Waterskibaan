67af631d02790bcf861ee1ab16c30ce9b59903f9
Joppe van Esch <bibijoppe@gmail.com>
2019-09-17 16:22:42 +0200

    Added the First Classes

--- Waterskibaan/Waterskibaan/Program.cs
+++ Waterskibaan/Waterskibaan/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,42 @@ namespace Waterskibaan
         static void Main(string[] args)
         {
         }
+    }
+
+    class Sporter
+    {
+        public int AantalRondenNogTeGaan { get; set; }
+        public Zwemvest Zwemvest { get; set; }
+        public Skies Skies { get; set; }
+        public Color KledingKleur { get; set; }
+        public List<IMoves> MyProperty { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+
+        }
+    }
+
+    interface IMoves
+    {
+        public int Move()
+        {
+            return 1;
+        }
+    }
+
+    class Lijn
+    {
+        public int PositieOpDeKabel;
+    }
+
+    class Skies
+    {
+
+    }
+
+    class Zwemvest
+    {
+
     }
 }
